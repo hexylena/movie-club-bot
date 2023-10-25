b94a349ec9e90fb563fb62d052df87d8158e820f
Helena Rasche <hexylena@galaxians.org>
2023-10-25 12:03:25 +0200

    fix datetimes

--- web/management/commands/fetch_imdb.py
+++ web/management/commands/fetch_imdb.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Permission
+from django.utils.timezone import now
+import time
 import math
 import random
 import re
@@ -11,7 +13,7 @@ class Command(BaseCommand):
     help = "Update existing user permissions"
 
     def handle(self, *args, **options):
-        years_2 = (datetime.datetime.now() - datetime.timedelta(days=365 * 2)).year
+        years_2 = (now() - datetime.timedelta(days=365 * 2)).year
 
         # There's no point in checking anything over 2 years old, it
         # probably hasn't changed much since we imported it.
@@ -26,11 +28,13 @@ class Command(BaseCommand):
             if unwatched.imdb_update is None:
                 days_since_updated = 730
             else:
-                days_since_updated = (datetime.datetime.now() - unwatched.imdb_update).days
+                days_since_updated = (now() - unwatched.imdb_update).days
 
             chance = 1 / (1 + math.exp(-0.01 * (days_since_updated - 730)))
 
-            if random.random() < chance:
+            if random.random() < chance * 10:
                 print(f"Updating {unwatched.title}")
                 unwatched.update_from_imdb()
                 time.sleep(1)
+            else:
+                print(f"Skipping {unwatched.title} d={days_since_updated} c={chance}")

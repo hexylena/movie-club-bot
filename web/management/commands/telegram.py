75a4e8b2ae6e6b3a8e9389b1d7425ed56dd107a3
Helena Rasche <hexylena@galaxians.org>
2025-02-02 10:28:12 +0100

    Fixes #35

--- web/management/commands/telegram.py
+++ web/management/commands/telegram.py
@@ -470,7 +470,7 @@ class Command(BaseCommand):
                 movie_details = json.loads(movie.meta)
                 days_ago = datetime.datetime.now().replace(tzinfo=datetime.timezone.utc) - movie.added
 
-                resp = f"Suggested by {movie.suggested_by} on {movie.added.strftime('%B %m, %Y')} ({str(days_ago)})\nVotes: "
+                resp = f"Suggested by {movie.suggested_by} on {movie.added.strftime('%B %d, %Y')} ({str(days_ago)})\nVotes: "
                 for v in movie.interest_set.all():
                     resp += f"{v.score_e}"
 

c7a6e9984ee117ece36acffe97161c64a87acd1a
Helena Rasche <hexylena@galaxians.org>
2025-05-05 11:21:39 +0200

    add tmdb updater

--- web/management/commands/fetch_imdb.py
+++ web/management/commands/fetch_imdb.py
@@ -32,11 +32,9 @@ class Command(BaseCommand):
             chance = 1 / (1 + math.exp(-0.01 * (days_since_updated - 730)))
 
             if random.random() < chance * 10:
-                print(f"Updating {unwatched.title}")
+                print(f"[IMDB] Updating {unwatched.title}")
                 unwatched.update_from_imdb()
                 time.sleep(1)
-            else:
-                print(f"Skipping {unwatched.title} d={days_since_updated} c={chance}")
 
 
         for missing_tmdb in web.models.MovieSuggestion.objects.filter(tmdb_id=None):
@@ -60,4 +58,26 @@ class Command(BaseCommand):
                 missing_tmdb.tmdb_id = get_tmdb_id(missing_tmdb.imdb_id)
                 if missing_tmdb.tmdb_id is not None:
                     missing_tmdb.save()
+                time.sleep(1)
+
+        # There's no point in checking anything over 2 years old, it
+        # probably hasn't changed much since we imported it.
+        for unwatched in web.models.MovieSuggestion.objects.filter(status=0, year__gt=years_2):
+            # calculate whether or not to refresh based on age.
+
+            # Generate a % chance to refresh based on age
+            # where 0 days since updated = 0%
+            # 730 days since updated = 50%
+            # Should use a sigmoid.
+
+            if unwatched.imdb_update is None:
+                days_since_updated = 730
+            else:
+                days_since_updated = (now() - unwatched.imdb_update).days
+
+            chance = 1 / (1 + math.exp(-0.01 * (days_since_updated - 730)))
+
+            if random.random() < chance * 10:
+                print(f"[TMDB] Updating {unwatched.title}")
+                unwatched.update_from_tmdb()
                 time.sleep(1)
